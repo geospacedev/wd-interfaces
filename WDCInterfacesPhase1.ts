4a58b1fdd69a41b909d9f9037448706e25632274
brent185 <asplundb@hotmail.com>
2025-10-09 14:55:24 -0500

    updated the perimeter type

--- WDCInterfacesPhase1.ts
+++ WDCInterfacesPhase1.ts
@@ -11,6 +11,6 @@ export interface ICaptureLevel {
     id: string;
     name: string;
     elevation: number;
-    walls: number[][]; // array of wall roots, each root is array of points [x1, y1, x2, y2]
-    perimeter: number[]; // polygon of the floor
+    walls: number[][]; // array of wall roots, each root is array of points [[x1, y1, x2, y2], [x3, y3, x4, y4], ...]
+    perimeter: number[][]; // polygon of the floor [[x1, y1], [x1, y1], ...]
 }
